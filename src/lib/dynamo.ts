9f5e24c5e8a8b467089ac92efd09a631b10c4950
David Messias Filho <david.messias.filho@tui.nl>
2025-08-25 17:10:57 +0200

    adjusted retricted environment variable name.

--- src/lib/dynamo.ts
+++ src/lib/dynamo.ts
@@ -1,7 +1,7 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 
-const client = new DynamoDBClient({ region: process.env.AWS_REGION });
+const client = new DynamoDBClient({ region: process.env.FINANCE_AWS_REGION });
 export const ddb = DynamoDBDocumentClient.from(client, {
   marshallOptions: { removeUndefinedValues: true }
 });
